c2fbbfbb0fb16830b1d3565b39e15d59da553c56
Marcos Júnior <marcosajr11@gmail.com>
2025-08-16 22:38:45 -0300

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let friends = [];
+
+function atualizarListaAmigo(listId, friends) {
+    const list = document.getElementById(listId);
+    list.innerHTML = "";
+    friends.forEach(function (friend) {
+        const li = document.createElement("li");
+        li.textContent = friend;
+        list.appendChild(li);
+    })
+}
+
+function adicionarAmigo() {
+    const name = document.getElementById("amigo").value;
+    if (name.trim() === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    friends.push(name);
+    document.getElementById("amigo").value = "";
+
+    atualizarListaAmigo("listaAmigos", friends);
+}
