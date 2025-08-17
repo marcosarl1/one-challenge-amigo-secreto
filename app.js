2341c36611443aa75ed0023588ce5c3affc5053a
Marcos Júnior <marcosajr11@gmail.com>
2025-08-16 23:05:49 -0300

    feat: Improvements on add friend and sorting function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let friends = [];
 
-function atualizarListaAmigo(listId, friends) {
-    const list = document.getElementById(listId);
+function atualizarListaAmigo(friends) {
+    const list = document.getElementById("listaAmigos");
     list.innerHTML = "";
     friends.forEach(function (friend) {
         const li = document.createElement("li");
@@ -11,13 +11,28 @@ function atualizarListaAmigo(listId, friends) {
 }
 
 function adicionarAmigo() {
-    const name = document.getElementById("amigo").value;
-    if (name.trim() === "") {
+    const input = document.getElementById("amigo");
+    const name = input.value.trim();
+    if (!name) {
         alert("Por favor, insira um nome.");
         return;
     }
-    friends.push(name);
-    document.getElementById("amigo").value = "";
 
-    atualizarListaAmigo("listaAmigos", friends);
+    friends.push(name);
+    input.value = "";
+    atualizarListaAmigo(friends);
+}
+
+function sortearAmigo() {
+    if (friends.length === 0) {
+        alert("Adicione amigos antes de sortear");
+        return;
+    }
+    const indice = Math.floor(Math.random() * friends.length);
+    const amigoSorteado = friends[indice];
+
+    const result = document.getElementById("resultado");
+    if (!result) return
+
+    result.textContent = "O amigo sorteado é: " + amigoSorteado;
 }
